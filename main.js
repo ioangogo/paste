7e16886d05a60c0afaf5283931c6399fb519915a
Phillip Tribble <phillip.tribble@gmail.com>
2016-02-07 00:30:15 -0800

    Fix

--- main.js
+++ main.js
@@ -80,48 +80,33 @@ var begin_share_message = ( '<html> <head> <link rel="stylesheet" href="//code.j
 var end_share_mesage = ( '</div></p></body></html>'
     );
 
-
-function uuid() {
-    var id = "";
-    var length = 10;
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for(var i = 0; i < length; i++) {
-        id += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return(id);
-}
-
-function send_post(id,paste_data){
-    var temp_paste_data = "";
-
-    if(paste_data.indexOf('\'') != -1){
-        temp_paste_data = paste_data.replace(/\'/g,"\\\'");
-        paste_date = temp_paste_data;
-    }
-
-    mysql_connection.getConnection(function(err,connection) {
-        mysql_connection.query('insert into paste (id,item) values("' + id + '","' + paste_data + '");', function(err,rows) {
-            if(err) {
-                console.log('Error sending paste data', err);
-            }
-            connection.release();
-        });
-    });
-}
-
 app.get('/paste/newpaste', function(req, res){
- var id = uuid();
- var paste_data = req.query['text'];
- send_post(id,paste_data);
- res.end(begin_share_message + '<h3 class="ui-widget-header">Your paste is ready to share!</h3>'+ 'Your paste is available here:<br><br><a href="' + site_name + '/show?id=' + id + '">' + site_name  + '/show?id=' + id + '</a>' + end_share_mesage);
+  var paste_data = req.query['text'];
+  var temp_paste_data = paste_data;
+  var length = 10;
+  var id = "";
+
+  if(paste_data.indexOf('\'') != -1){
+    paste_data = temp_paste_data.replace(/\'/g,"\\\'");
+}
+
+var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+for(var i = 0; i < length; i++) {
+    id += possible.charAt(Math.floor(Math.random() * possible.length));
+}
+
+mysql_connection.getConnection(function(err,connection) {
+    mysql_connection.query("insert into paste (id,item) values('" + id + "','" + paste_data + "');", function(err,rows) {
+        if(err) {
+            console.log('Error sending paste data', err);
+        }
+        connection.release();
+    });
+  });
+
+    res.end(begin_share_message + '<h3 class="ui-widget-header">Your paste is ready to share!</h3>'+ 'Your paste is available here:<br><br><a href="' + site_name + '/show?id=' + id + '">' + site_name  + '/show?id=' + id + '</a>' + end_share_mesage);
 });
 
-app.get('/paste/cli', function(req, res){
- var id = uuid();
- var paste_data = req.query['text'];
- send_post(id,paste_data);
- res.end('<html>Your paste is available here: '+ site_name + '/show?id=' + id + '</html>');
-});
 
 app.get('/paste/new', function(req, res){
     var responseString = "";
