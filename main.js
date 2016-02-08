87e31f3c9ca32ba6739db1b534f6c6b0cb0758d8
Phillip Tribble <phillip.tribble@gmail.com>
2016-02-07 21:28:51 -0800

    New feature

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function createtable_async(data) {
 
 function createdb(callback){
     mysql_connection_create.getConnection(function(err,connection) {
-     mysql_connection_create.query("create database " + database + ";", function(err, rows) {
+       mysql_connection_create.query("create database " + database + ";", function(err, rows) {
         if(err) {
             console.log('Error creating database', err);
         }
@@ -33,7 +33,7 @@ function createdb(callback){
         createtable(createtable_async);
         callback('Creating Database.....');
     });
- });
+   });
 }
 
 function createtable(callback){
@@ -68,8 +68,8 @@ var mini_begin_share_message = ( '<html> <head> <link rel="stylesheet" href="//c
     + '<link rel="stylesheet" href="/resources/demos/style.css">'
     + '<script>'
     + ' $(function() {'
-     + '      $( "#dialog" ).dialog();'
-     + '  });'
+       + '      $( "#dialog" ).dialog();'
+       + '  });'
 + ' </script>'
 + '</head>'
 + '<body>'
@@ -82,8 +82,8 @@ var begin_share_message = ( '<html> <head> <link rel="stylesheet" href="//code.j
     + '<link rel="stylesheet" href="/resources/demos/style.css">'
     + '<script>'
     + ' $(function() {'
-     + '      $( "#dialog" ).dialog();'
-     + '  });'
+       + '      $( "#dialog" ).dialog();'
+       + '  });'
 + ' </script>'
 + '</head>'
 + '<body>'
@@ -94,6 +94,25 @@ var begin_share_message = ( '<html> <head> <link rel="stylesheet" href="//code.j
 
 var end_share_mesage = ( '</div></p></body></html>'
     );
+
+app.get('/paste/submitedit', function(req, res){
+  var paste_data = req.query['text'];
+  var id = req.query['id'];
+
+  mysql_connection.getConnection(function(err,connection) {
+    var safe_paste_data = mysql.escape(paste_data);
+    console.log("update paste set item=" + safe_paste_data + " where id='" + id + "';");
+    mysql_connection.query("update paste set item=" + safe_paste_data + " where id='" + id + "';", function(err,rows) {
+        if(err) {
+            console.log('Error sending paste data', err);
+        }
+        connection.release();
+    });
+});
+
+  res.end(mini_begin_share_message + '<h3 class="ui-widget-header">Your paste is ready to share!</h3>'+ 'Your paste has been edited:<br><br><a href="' + site_name + '/show?id=' + id + '">' + site_name  + '/show?id=' + id + '</a>' + end_share_mesage);
+});
+
 
 app.get('/paste/newpaste', function(req, res){
   var paste_data = req.query['text'];
@@ -124,6 +143,38 @@ app.get('/paste/new', function(req, res){
     res.sendFile(__dirname + '/new.html');
 });
 
+
+
+app.get('/paste/edit', function(req, res){
+    var id = req.query['id'];
+    data = "";
+    mysql_connection.getConnection(function(err,connection) {
+        mysql_connection.query('select * from paste where id="' + id + '";', function(err, rows) { 
+            if (!err)  {
+                data = rows;
+                if(data.length > 0) {
+                    res.write(mini_begin_share_message + '<h3 class="ui-widget-header"> Edit Paste: ' + rows[0].id
+                        + '<form action="/paste/submitedit" id="usrform">'
+                        + '<p align=center>'
+                        + '<b>Paste here: </b><br><br>'
+                        + '<input type="hidden" name="id" value="' + rows[0].id + '">'
+                        + '<textarea rows="20" cols="100" name="text" form="usrform">' + rows[0].item + '</textarea>'
+                        + '<p align=center>'
+                        + '<input type="submit"></form>'
+                        + end_share_mesage);
+                } else {
+                   res.end(begin_share_message + '<h3 class="ui-widget-header">Error, paste does not exist!</h3>'+  end_share_mesage);
+               }
+           }else {
+            data =  "An error has occurred.";
+            console.log(err);
+        }
+        connection.release();
+    });
+});
+});
+
+
 app.get('/paste/show', function(req, res){
     var id = req.query['id'];
     data = "";
@@ -134,9 +185,9 @@ app.get('/paste/show', function(req, res){
                 if(data.length > 0) {
                     res.end(begin_share_message + '<h3 class="ui-widget-header">Viewing shared post: ' + rows[0].id + '</h3>'+ rows[0].item + end_share_mesage);
                 } else {
-                 res.end(begin_share_message + '<h3 class="ui-widget-header">Error, paste does not exist!</h3>'+  end_share_mesage);
-             }
-         }else {
+                   res.end(begin_share_message + '<h3 class="ui-widget-header">Error, paste does not exist!</h3>'+  end_share_mesage);
+               }
+           }else {
             data =  "An error has occurred.";
             console.log(err);
         }
@@ -189,8 +240,7 @@ app.get('/paste/body', function(req, res){
             }
             connection.release();
             for (var i in data){
-                res.write(mini_begin_share_message + '<h3 class="ui-widget-header">Paste ID: <a href="' + site_name + '/delete?id=' + data[i].id + '&Submit=View"><img src="/paste/delete.png" height="10" width="10"></a> <a href="' + site_name + '/show?id=' + data[i].id + '&Submit=View">' + data[i].id + '</a>' + '.</h3>' +  data[i].item + end_share_mesage);
-
+                res.write(mini_begin_share_message + '<h3 class="ui-widget-header">Paste ID: <a href="' + site_name + '/show?id=' + data[i].id + '&Submit=View">' + data[i].id + '</a><a href="' + site_name + '/delete?id=' + data[i].id + '&Submit=View"><img src="/paste/delete.png" height="10" width="10"></a> <a href="' + site_name + '/edit?id=' + data[i].id + '&Submit=View"><img src="/paste/edit.png" height="10" width="10"></a></h3>' +  data[i].item + end_share_mesage);
             }
             res.end('</html>');
         });
@@ -221,6 +271,10 @@ app.get('/paste/delete.png', function(req, res){
 
 app.get('/paste/caring.png', function(req, res){
     res.sendFile(__dirname + '/caring.png');
+});
+
+app.get('/paste/edit.png', function(req, res){
+    res.sendFile(__dirname + '/edit.png');
 });
 
 app.get('/paste/from.png', function(req, res){
